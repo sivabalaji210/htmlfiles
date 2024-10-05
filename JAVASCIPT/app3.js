b26c1148548b6c714a405e5f2d5653364f210d0a
sivabalaji210 <balajilogics@gmail.com>
2024-10-05 23:20:48 +0530

    arrow functions in javascript

---
+++ JAVASCIPT/app3.js
@@ -0,0 +1,18 @@
+let add=(n1,n2)=>
+{
+    console.log(n1+n2)
+}
+add(3,4)
+ 
+let oddeven=(num)=>{
+    if(num%2==0)
+    {
+        console.log("even")
+    }
+    else
+    {
+        console.log("odd")
+    }
+
+}
+oddeven(10)
