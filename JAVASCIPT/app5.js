06be425701d1bb5af150adf3f70e1449e614f77d
sivabalaji210 <balajilogics@gmail.com>
2024-10-07 22:25:57 +0530

    destructring in javasecipt

---
+++ JAVASCIPT/app5.js
@@ -0,0 +1,13 @@
+console.log("array destructring")
+let arr=[3,6,2,10]
+let  [ele1,ele2,ele3,ele4]=arr
+ console.log(ele1,ele2,ele3,ele4)
+
+ let course={
+    'name':'balaji',
+    'quqlification':'b.tech',
+    'age':'12'
+ }
+ let {name,quqlification,age}=course
+
+ console.log(name,quqlification,age)
